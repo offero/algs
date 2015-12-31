5c825f1ea35f67970ef1a4a04d850bcd564ffb56
Chris <offero@wheeljack>
2015-12-31 09:45:28 -0500

    Working Binary Indexed Tree and 2-inversion counting with it.

--- binary_indexed_tree.py
+++ binary_indexed_tree.py
@@ -15,23 +15,40 @@ def parentgt(idx):
     return idx + (idx & -idx)
 
 class BIT(object):
-    def __init__(self, arr):
-        """arr: The list of numbers for which we create the BIT."""
-        self._bit = [0] * (len(arr)+1)  # list of 0s
+    def __init__(self, arr=None, n=0):
+        """Create a Binary Indexed Tree from list `arr`.
+
+        Running time: O(n*log(n))
+        """
+        if arr is not None:
+            n = len(arr)
+        else:
+            arr = []
+
+        self._bit = [0] * (n+1)  # list of 0s
+
         for i in range(1, len(arr)+1):
             self.update_value(i, arr[i-1])
 
     def update_value(self, idx, val):
-        """
+        """Add `val` to the value at BIT index `idx`.
+
         idx: BIT index (list index + 1)
         val: +/- amount (Amount of change of value at index i).
+
+        Running time: O(log(n))
         """
         while idx < len(self._bit):
             self._bit[idx] += val
             idx = parentgt(idx)
 
     def get_sum(self, idx):
-        """Return the sum up to BIT index idx (list index + 1)."""
+        """Return the sum up to BIT index `idx` (list index + 1).
+
+        idx: BIT index (list index + 1)
+
+        Running time: O(log(n))
+        """
         tot = 0
         while idx > 0:
             tot += self._bit[idx]
--- inversioncount.py
+++ inversioncount.py
@@ -3,6 +3,8 @@ form an inversion of size 3 if a[i] > a[j] >a[k] and i < j < k. Find total
 number of inversions of size 3.
 """
 
+from binary_indexed_tree import BIT
+
 def three_inversions_ctlower(arr):
     """
     Count the number of 3 inversions by counting the number of elements
@@ -21,6 +23,39 @@ def three_inversions_ctlower(arr):
                 ct += less[j]
     return ct
 
+def values_to_indices(arr):
+    """For each element of the input list, replace it with it's index position
+    in the sorted version of the input list. This maintains the inversion
+    relationships and turns the list into all positive integers.
+
+    Pseudocode: arr[i] = indexof(sorted(arr), i)
+
+    Running time: O(n*log(n)) dominated by the sort
+    Extra space: O(n)
+    """
+    idxarr = [0] * len(arr)
+    sorted_array = sorted(arr)
+    idxs = {}
+    for i, val in enumerate(sorted_array):
+        if val not in idxs:
+            idxs[val] = i
+    for i, val in enumerate(arr):
+        idxarr[i] = idxs[val]
+        idxs[val] += 1
+    return idxarr
+
+def inversion_count_bit(arr):
+    """Counts the number of 2-inversions using a Binary Indexed Tree"""
+    iarr = values_to_indices(arr)
+    n = len(arr)
+    bit = BIT(n=n)
+    invs = 0
+    for i in range(n-1, -1, -1):
+        bitidx = iarr[i]+1
+        bit.update_value(bitidx, 1)
+        print (bit._bit)
+        invs += bit.get_sum(bitidx-1)
+    return invs
 
 def test_1():
     examples = [
